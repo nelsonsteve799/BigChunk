2dc18dbd224286e0bed5d3e736b3ea4b166aaa03
Nelson Steve <nelsonsteve799@gmail.com>
2022-07-11 15:47:57 +0500

    Setup the project folders.

---
+++ BigChunk/src/Test.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace BigChunk {
+
+	__declspec(dllexport) void Print();
+
+}
---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,11 @@
+
+
+namespace BigChunk {
+
+	__declspec(dllimport) void Print();
+
+}
+
+void main() {
+	BigChunk::Print();
+}
