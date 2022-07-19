60b1881e7fcde70e9a73ae2e488719455aea7c43
Nelson Steve <nelsonsteve799@gmail.com>
2022-07-19 14:23:54 +0500

    added premake

---
+++ premake5.lua
@@ -0,0 +1,97 @@
+workspace "BigChunk"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "BigChunk"
+	location "BigChunk"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+	defines{
+		"BC_PLATFORM_WINDOWS",
+		"BC_BUILD_DLL"
+	}
+
+	postbuildcommands{
+		("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+	}
+
+	filter "configurations:Debug"
+		defines "BC_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "BC_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "BC_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs{
+		"BigChunk/vendor/spdlog/include",
+		"BigChunk/src"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+	defines{
+		"BC_PLATFORM_WINDOWS",
+	}
+
+	links{
+		"BigChunk"
+	}
+
+	filter "configurations:Debug"
+		defines "BC_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "BC_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "BC_DIST"
+		optimize "On"
